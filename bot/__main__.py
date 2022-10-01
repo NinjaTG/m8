f13aa26aecbaab5e63370a3c4c0d3a40940e0893
Ninja Hound <60533346+NinjaTG@users.noreply.github.com>
2022-10-01 10:00:44 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -48,15 +48,15 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/z_mirror")
+    buttons.buildbutton("Its meh", "https://t.me/telegram")
+    buttons.buildbutton("Powered by", "https://t.me/TGFilmsZone")
+    #buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
+    #buttons.buildbutton("Owner", "https://t.me/z_mirror")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-Welcome | {TITLE_NAME} BOT is ready for you
-Type /{BotCommands.HelpCommand} to get a list of available commands
+
+🦜 This is Leech Mirror TG Bot with cool Features 😊
                         '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
